b440c5fa712252ec2e57c159e01b116efcf31d8f
Supratim Chakraborty <sup270792@gmail.com>
2015-05-10 02:15:04 +0530

    Sensor Data POJO

--- app/src/main/java/com/airaroundme/airaroundme/MainActivityFragment.java
+++ app/src/main/java/com/airaroundme/airaroundme/MainActivityFragment.java
@@ -1,10 +1,13 @@
 package com.airaroundme.airaroundme;
 
-import android.support.v4.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 
 /**
@@ -12,12 +15,32 @@ import android.view.ViewGroup;
  */
 public class MainActivityFragment extends Fragment {
 
+    private LinearLayout mainView;
+    private TextView air_around_me_button;
+
     public MainActivityFragment() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        mainView = (LinearLayout)inflater.inflate(R.layout.fragment_main, container, false);
+        air_around_me_button = (TextView)mainView.findViewById(R.id.air_around_button);
+        return mainView;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        final Intent intent = new Intent(getActivity(),SensorResponseActivity.class);
+
+        air_around_me_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getActivity().startActivity(intent);
+            }
+        });
+
+
     }
 }
