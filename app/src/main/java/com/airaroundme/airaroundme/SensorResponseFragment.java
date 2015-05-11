110e79eb79315dfb69d284f3fd36869bd9591a52
Supratim Chakraborty <sup270792@gmail.com>
2015-05-11 10:12:17 +0530

    Added a loader

--- app/src/main/java/com/airaroundme/airaroundme/SensorResponseFragment.java
+++ app/src/main/java/com/airaroundme/airaroundme/SensorResponseFragment.java
@@ -6,14 +6,15 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 
 import com.airaroundme.airaroundme.adapters.SensorResponseAdapter;
 import com.airaroundme.airaroundme.asyncTasks.HttpAsyncTask;
 import com.airaroundme.airaroundme.constants.Constants;
 import com.airaroundme.airaroundme.interfaces.AsyncResponse;
 import com.airaroundme.airaroundme.objects.SensorData;
+import com.github.rahatarmanahmed.cpv.CircularProgressView;
 import com.google.gson.Gson;
 
 
@@ -23,12 +24,13 @@ import com.google.gson.Gson;
 public class SensorResponseFragment extends Fragment {
 
 
-    private LinearLayout mainView;
+    private RelativeLayout mainView;
     private ListView sensor_response_listview;
     private SensorResponseAdapter adapter;
     private Context mContext;
     private String jsonStr;
     private SensorData data;
+    private CircularProgressView progressView;
 
 
     public SensorResponseFragment() {
@@ -39,8 +41,11 @@ public class SensorResponseFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         mContext = getActivity();
-        mainView = (LinearLayout) inflater.inflate(R.layout.fragment_sensor_response, container, false);
+        mainView = (RelativeLayout) inflater.inflate(R.layout.fragment_sensor_response, container, false);
         sensor_response_listview = (ListView) mainView.findViewById(R.id.sensor_response_listview);
+        progressView = (CircularProgressView) mainView.findViewById(R.id.progress_view);
+        progressView.setVisibility(View.VISIBLE);
+        progressView.startAnimation();
         getResponseFromSensor();
 
         return mainView;
@@ -52,8 +57,29 @@ public class SensorResponseFragment extends Fragment {
             public void processFinish(Object output) {
                 jsonStr = (String)output;
                 data = new Gson().fromJson(jsonStr,SensorData.class);
-                adapter = new SensorResponseAdapter(mContext,data);
-                sensor_response_listview.setAdapter(adapter);
+                if(data!=null){
+                    progressView.setVisibility(View.GONE);
+                    adapter = new SensorResponseAdapter(mContext,data);
+                    sensor_response_listview.setAdapter(adapter);
+                }
+                else{
+                    Thread thread = new Thread(){
+                        @Override
+                        public void run() {
+                           try{
+                               synchronized (this) {
+                                   wait(2000);
+                               }
+                           }
+                           catch (InterruptedException e){
+                               //TODO
+                           }
+                        }
+                    };
+
+                    thread.start();
+                }
+
             }
         });
         httpAsyncTask.execute(Constants.urlSensor);
