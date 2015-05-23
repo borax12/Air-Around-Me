f2b1cc6c8f3b2570959278e5362c5f310daee0d7
Supratim Chakraborty <sup270792@gmail.com>
2015-05-24 04:11:49 +0530

    Prediction Page done

--- app/src/main/java/com/airaroundme/airaroundme/PredictionResponseFragment.java
+++ app/src/main/java/com/airaroundme/airaroundme/PredictionResponseFragment.java
@@ -1,12 +1,8 @@
 package com.airaroundme.airaroundme;
 
 import android.content.Context;
-import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.Typeface;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
@@ -17,7 +13,6 @@ import android.view.ViewGroup;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
 import android.view.animation.ScaleAnimation;
-import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -26,50 +21,34 @@ import android.widget.Toast;
 import com.airaroundme.airaroundme.asyncTasks.HttpAsyncTask;
 import com.airaroundme.airaroundme.constants.Constants;
 import com.airaroundme.airaroundme.interfaces.AsyncResponse;
-import com.airaroundme.airaroundme.objects.FetchedData;
-import com.airaroundme.airaroundme.objects.Metrics;
-import com.airaroundme.airaroundme.objects.Station;
+import com.airaroundme.airaroundme.objects.Aqi;
 import com.github.rahatarmanahmed.cpv.CircularProgressView;
 import com.google.gson.Gson;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
 
 
 /**
  * A placeholder fragment containing a simple view.
  */
-public class SensorResponseFragment extends Fragment implements LocationListener{
+public class PredictionResponseFragment extends Fragment{
 
 
     private RelativeLayout mainView;
     private Context mContext;
     private String jsonStr;
     private CircularProgressView progressView;
-    private LocationManager mLocationManager;
-    private List<Station> stationList;
-    private int SIZE=3;
-    private TreeMap<Float,Station> stationToDistance = new TreeMap<Float,Station>();
-    private Station mStation;
-    private boolean firstTime=true;
-    private FetchedData data;
+    private int stationId;
+    private Aqi data;
     private TextView airQualityLine;
     private TextView airQualityTextValue;
     private Typeface font;
-    private Location mLocation;
     private TextView airQualityValue;
     private LinearLayout aqiBar;
     private LinearLayout mainBarContainer;
     private LinearLayout proTipContainer;
     private TextView proTipBulb;
     private TextView proTipText;
-    private Button moreButton;
-    private Button predictionButton;
 
-    public SensorResponseFragment() {
+    public PredictionResponseFragment() {
 
     }
 
@@ -77,10 +56,7 @@ public class SensorResponseFragment extends Fragment implements LocationListener
     public void onResume() {
         super.onResume();
         if(data!=null){
-            float scaleFactor = Float.parseFloat(data.getAqi().getValue())/500.0f;
-            String remark = data.getAqi().getRemark();
-
-            //setting up the background color
+            float scaleFactor = Float.parseFloat(data.getValue())/500.0f;
 
             int newWidth = 100+ (int) (aqiBar.getWidth()*scaleFactor);
             int originalHeight = aqiBar.getHeight();
@@ -97,102 +73,56 @@ public class SensorResponseFragment extends Fragment implements LocationListener
             animation.setDuration(1000);
 
             proTipContainer.setAnimation(animation);
-            moreButton.setAnimation(animation);
-            predictionButton.setAnimation(animation);
         }
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        stationList = new ArrayList<>(SIZE);
+        stationId = getActivity().getIntent().getExtras().getInt("stationId");
 
-        //Creating the stations
-        Station stationBTM = new Station(808,12.912811,77.609218);
-        Station stationPeenya = new Station(809,13.033836,77.513245);
-        Station stationBSSW = new Station(810,12.938932,77.697271 );
-
-        //adding to the list
-        stationList.add(stationBTM);
-        stationList.add(stationPeenya);
-        stationList.add(stationBSSW);
-
-        getCurrentLocation();
-    }
-
-    public void getCurrentLocation() {
-        mLocationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
-        mLocation = mLocationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-        if(mLocation!=null){
-            chooseNearestStation();
-        }
-        else{
-            mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         mContext = getActivity();
-        mainView = (RelativeLayout) inflater.inflate(R.layout.fragment_sensor_response, container, false);
+        mainView = (RelativeLayout) inflater.inflate(R.layout.fragment_prediction_response, container, false);
         progressView = (CircularProgressView) mainView.findViewById(R.id.progress_view);
         progressView.setVisibility(View.VISIBLE);
         progressView.startAnimation();
 
-        airQualityLine =(TextView) mainView.findViewById(R.id.air_quality_line);
-        airQualityTextValue = (TextView)mainView.findViewById(R.id.air_quality_text);
-        airQualityValue = (TextView) mainView.findViewById(R.id.aqi_value);
-        aqiBar = (LinearLayout) mainView.findViewById(R.id.aqi_bar);
-        mainBarContainer = (LinearLayout) mainView.findViewById(R.id.main_bar_container);
-        proTipContainer = (LinearLayout) mainView.findViewById(R.id.pro_tip);
-        proTipBulb = (TextView) mainView.findViewById(R.id.pro_tip_bulb);
-        proTipText = (TextView) mainView.findViewById(R.id.pro_tip_text);
-        moreButton = (Button) mainView.findViewById(R.id.more_button);
-        predictionButton = (Button) mainView.findViewById(R.id.prediction_button);
+        airQualityLine =(TextView) mainView.findViewById(R.id.air_quality_line_prediction);
+        airQualityTextValue = (TextView)mainView.findViewById(R.id.air_quality_text_prediction);
+        airQualityValue = (TextView) mainView.findViewById(R.id.aqi_value_prediction);
+        aqiBar = (LinearLayout) mainView.findViewById(R.id.aqi_bar_prediction);
+        mainBarContainer = (LinearLayout) mainView.findViewById(R.id.main_bar_container_prediction);
+        proTipContainer = (LinearLayout) mainView.findViewById(R.id.pro_tip_prediction);
+        proTipBulb = (TextView) mainView.findViewById(R.id.pro_tip_bulb_prediction);
+        proTipText = (TextView) mainView.findViewById(R.id.pro_tip_text_prediction);
 
 
         font = Typeface.createFromAsset(getActivity().getAssets(),"Roboto-Light.ttf");
         airQualityLine.setTypeface(font);
         airQualityValue.setTypeface(font);
         proTipText.setTypeface(font);
-        moreButton.setTypeface(font);
-        predictionButton.setTypeface(font);
 
         font = Typeface.createFromAsset(getActivity().getAssets(),"Roboto-Medium.ttf");
         airQualityTextValue.setTypeface(font);
         font = Typeface.createFromAsset(getActivity().getAssets(),"fontawesome-webfont.ttf");
         proTipBulb.setTypeface(font);
+
+        getResponseFromSensor();
         return mainView;
     }
 
-    public void chooseNearestStation() {
-
-        firstTime=false;
-        Double currentLat = mLocation.getLatitude();
-        Double currentLong = mLocation.getLongitude();
-
-        float results[] = new float[2];
-
-        for(Station station:stationList){
-            Location.distanceBetween(currentLat, currentLong, station.getLatitude(), station.getLongitude(), results);
-            stationToDistance.put(results[0],station);
-        }
-
-        Map.Entry<Float,Station> entry = stationToDistance.firstEntry();
-
-        mStation = entry.getValue();
-
-        getResponseFromSensor();
-
-    }
 
     private void getResponseFromSensor() {
         HttpAsyncTask httpAsyncTask = new HttpAsyncTask(new AsyncResponse() {
             @Override
             public void processFinish(Object output) {
                 jsonStr = (String)output;
-                data = new Gson().fromJson(jsonStr,FetchedData.class);
+                data = new Gson().fromJson(jsonStr,Aqi.class);
                 if(jsonStr!=null){
                     progressView.setVisibility(View.GONE);
                     setUpViews(data);
@@ -213,20 +143,21 @@ public class SensorResponseFragment extends Fragment implements LocationListener
 
             }
         });
-        String urlBuilder = Constants.urlSensor;
-        urlBuilder =urlBuilder.concat("?stationId=" + mStation.getStationId());
+        String urlBuilder = Constants.predictionSensor;
+        urlBuilder =urlBuilder.concat("?stationId="+stationId);
         httpAsyncTask.execute(urlBuilder);
     }
 
-    private void setUpViews(FetchedData data) {
+    private void setUpViews(Aqi data) {
 
-        float scaleFactor = Float.parseFloat(data.getAqi().getValue())/500.0f;
-        String remark = data.getAqi().getRemark();
+        float scaleFactor = Float.parseFloat(data.getValue())/500.0f;
+        String remark = data.getRemark();
 
+        airQualityLine.setVisibility(View.VISIBLE);
         //setting up the background color
-        mainView.setBackgroundColor(Color.parseColor(data.getAqi().getColor()));
+        mainView.setBackgroundColor(Color.parseColor(data.getColor()));
         airQualityTextValue.setText(remark.toUpperCase());
-        airQualityValue.setText(data.getAqi().getValue());
+        airQualityValue.setText(data.getValue());
 
         switch (remark){
             case "Good": proTipText.setText("Go take a walk");
@@ -264,58 +195,6 @@ public class SensorResponseFragment extends Fragment implements LocationListener
 
         proTipContainer.setAnimation(animation);
 
-        moreButton.setVisibility(View.VISIBLE);
-        predictionButton.setVisibility(View.VISIBLE);
-        moreButton.setAnimation(animation);
-        predictionButton.setAnimation(animation);
-
-        final Intent intent = new Intent(getActivity(),DetailedActivity.class);
-        final Intent intent_prediction = new Intent(getActivity(),PredictionResponseActivity.class);
-
-         Metrics[] valueArray = data.getMetrics();
-        ArrayList<Metrics> valueList = new ArrayList<>(Arrays.asList(valueArray));
-
-        intent.putExtra("color",data.getAqi().getColor());
-        intent.putExtra("values",valueList);
-        intent_prediction.putExtra("stationId",mStation.getStationId());
-
-        moreButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                getActivity().startActivity(intent);
-            }
-        });
-
-        predictionButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                getActivity().startActivity(intent_prediction);
-            }
-        });
-
-
     }
 
-
-    @Override
-    public void onLocationChanged(Location location) {
-            mLocation = location;
-            chooseNearestStation();
-            mLocationManager.removeUpdates(this);
-    }
-
-    @Override
-    public void onStatusChanged(String provider, int status, Bundle extras) {
-
-    }
-
-    @Override
-    public void onProviderEnabled(String provider) {
-
-    }
-
-    @Override
-    public void onProviderDisabled(String provider) {
-
-    }
 }
